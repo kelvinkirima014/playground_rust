df4e14bf27584f96b2007b3f477d18af8871b838
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-07-16 23:59:18 +0300

    added ownership die

---
+++ ownership/target/debug/deps/ownership-c312fdd839945c1b.d
@@ -0,0 +1,5 @@
+/home/kelvin/Desktop/rustProj/rust_playground/ownership/target/debug/deps/ownership-c312fdd839945c1b: src/main.rs
+
+/home/kelvin/Desktop/rustProj/rust_playground/ownership/target/debug/deps/ownership-c312fdd839945c1b.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/kelvin/Desktop/rustProj/rust_playground/ownership/target/debug/ownership: /home/kelvin/Desktop/rustProj/rust_playground/ownership/src/main.rs
