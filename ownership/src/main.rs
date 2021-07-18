b8e8a44491604453aa62b1f44127c25cdf5e531e
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-07-18 17:12:39 +0300

    updated ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,31 +1,41 @@
+/* Each value in Rust has a variable that’s called its owner.
+There can only be one owner at a time.
+When the owner goes out of scope, the value will be dropped.
+*/
 //Variables live in the scope of the function that
 //creates them since the time of creation
 //variables remain valid until they go out of scope
 //some data types like integers and bool are stored in the stack
 //other complex ones like String are stored in the heap
     
+
 fn main() {
-    let s = String::from("hello");  // s comes into scope
+    let s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
 
-    takes_ownership(s);             // s's value moves into the function...
-                                    // ... and so is no longer valid here
+    let s2 = String::from("hello");     // s2 comes into scope
 
-    let x = 5;                      // x comes into scope
+    let s3 = takes_and_gives_back(s2); 
+    println!("{}", s3); // s2 is moved into
+                                        // takes_and_gives_back, which also
+                                        // moves its return value into s3
+} // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
+  // moved, so nothing happens. s1 goes out of scope and is dropped.
 
-    makes_copy(x);                  // x would move into the function,
-                                    // but i32 is Copy, so it's okay to still
-                                    // use x afterward
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
 
-} // Here, x goes out of scope, then s. But because s's value was moved, nothing
-  // special happens.
+    let some_string = String::from("hello"); // some_string comes into scope
 
-fn takes_ownership(some_string: String) { // some_string comes into scope
-    println!("{}", some_string);
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
+                                // some_string is returned and
+         some_string                                    // moves out to the calling
+                                             // function
+}
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-    println!("{}", some_integer);
-} // Here, some_integer goes out of scope. Nothing special happens.
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+                                                      // scope
 
-
+    a_string  // a_string is returned and moves out to the calling function
+}
