c0cccccf9600146bdf37fd8888730ce4257863a6
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-09-05 21:37:35 +0300

    add practice

---
+++ practice/target/debug/deps/practice-e4d53c48c78c5ce5.d
@@ -0,0 +1,5 @@
+/home/kelvin/Desktop/rustProj/rust_playground/practice/target/debug/deps/practice-e4d53c48c78c5ce5: src/main.rs
+
+/home/kelvin/Desktop/rustProj/rust_playground/practice/target/debug/deps/practice-e4d53c48c78c5ce5.d: src/main.rs
+
+src/main.rs:
---
+++ practice/target/debug/practice.d
@@ -0,0 +1,1 @@
+/home/kelvin/Desktop/rustProj/rust_playground/practice/target/debug/practice: /home/kelvin/Desktop/rustProj/rust_playground/practice/src/main.rs
