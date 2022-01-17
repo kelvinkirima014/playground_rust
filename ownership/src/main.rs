272f3120037cab958c4dcf5f856c0e76cf7f5b7b
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-01-17 14:41:34 +0300

    refs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,41 +1,9 @@
-/* Each value in Rust has a variable that’s called its owner.
-There can only be one owner at a time.
-When the owner goes out of scope, the value will be dropped.
-*/
-//Variables live in the scope of the function that
-//creates them since the time of creation
-//variables remain valid until they go out of scope
-//some data types like integers and bool are stored in the stack
-//other complex ones like String are stored in the heap
-    
-
 fn main() {
-    let s1 = gives_ownership();         // gives_ownership moves its return
-                                        // value into s1
-
-    let s2 = String::from("hello");     // s2 comes into scope
-
-    let s3 = takes_and_gives_back(s2); 
-    println!("{}", s3); // s2 is moved into
-                                        // takes_and_gives_back, which also
-                                        // moves its return value into s3
-} // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
-  // moved, so nothing happens. s1 goes out of scope and is dropped.
-
-fn gives_ownership() -> String {             // gives_ownership will move its
-                                             // return value into the function
-                                             // that calls it
-
-    let some_string = String::from("hello"); // some_string comes into scope
-
-                                // some_string is returned and
-         some_string                                    // moves out to the calling
-                                             // function
-}
-
-// takes_and_gives_back will take a String and return one
-fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
-                                                      // scope
-
-    a_string  // a_string is returned and moves out to the calling function
-}
+  let s = String::from("Test");
+  heap_example(&s);
+  }
+  fn heap_example(input: &String) {
+  let mystr = input;
+  //let _otherstr = mystr;
+  println!("{}", mystr);
+  }
---
+++ ownership/target/debug/deps/ownership-420efaea36c284d5.d
@@ -0,0 +1,5 @@
+/home/kelvin/Desktop/rustProj/rust_playground/ownership/target/debug/deps/ownership-420efaea36c284d5.rmeta: src/main.rs
+
+/home/kelvin/Desktop/rustProj/rust_playground/ownership/target/debug/deps/ownership-420efaea36c284d5.d: src/main.rs
+
+src/main.rs:
